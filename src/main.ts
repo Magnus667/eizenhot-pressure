2699331f060c11820db032a23778f09cd55bfa39
magnus <magnus.eisenhuth@y-com-automation.de>
2021-01-12 20:46:39 +0100

    First commit for adding frontend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const frontendDirectory = path.join(__dirname, '../../eizenhot-pressure-frontend/dist/eizenhot-pressure-frontend')
+  app.useStaticAssets(frontendDirectory);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
