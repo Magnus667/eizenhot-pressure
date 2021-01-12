282382ff07c4a2ee641e25632c989586a0a05567
magnus <magnus.eisenhuth@y-com-automation.de>
2021-01-12 21:46:22 +0100

    Fix the path, to make development and build work both.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+console.log(process.env.npm_lifecycle_event)
+
+// '..' is used for heroku. '../..' is used for development.
+const pathFix = process.env.npm_lifecycle_event !== "start" ? '../..' : '..';
+
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'eizenhot-pressure-frontend/dist/eizenhot-pressure-frontend')
+      rootPath: join(__dirname, pathFix, 'eizenhot-pressure-frontend/dist/eizenhot-pressure-frontend')
     })
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  // const frontendDirectory = path.join(__dirname, '../../eizenhot-pressure-frontend/dist/eizenhot-pressure-frontend')
-  // app.useStaticAssets(frontendDirectory);
-
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
